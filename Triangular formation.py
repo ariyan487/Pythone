ce8faac648b5bbeeb6dfd244e9e29b2c3e632c67
A　R　I　Y　A　 N　ツ <139285103+ariyan487@users.noreply.github.com>
2024-07-15 15:19:01 +0600

    Add files via upload

---
+++ Triangular formation.py
@@ -0,0 +1,28 @@
+import math
+
+# Taking user inputs
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+
+# Checking if triangle formation is possible
+if (a + b > c) and (b + c > a) and (c + a > b):
+    # Calculating the semi-perimeter
+    s = (a + b + c) / 2
+    # Calculating the area using Heron's formula
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    print(f"The area of the triangle is: {area:.2f}")
+else:
+    print("Triangular formation is not possible")
+
+
+
+
+
+
+
+
+
+
+
+
