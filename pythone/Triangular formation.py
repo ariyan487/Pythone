90f27e5d8457c00020c84dca7af36ee6bc83bb50
A　R　I　Y　A　 N　ツ <139285103+ariyan487@users.noreply.github.com>
2024-07-15 15:47:29 +0600

    Update Triangular formation.py

--- pythone/Triangular formation.py
+++ pythone/Triangular formation.py
@@ -13,19 +13,7 @@ if (a + b > c) and (b + c > a) and (c + a > b):
     area = math.sqrt(s * (s - a) * (s - b) * (s - c))
     print(f"The area of the triangle is: {area:.2f}")
 else:
-    print("Triangular formation is not possible")
-
-
-a = float(input("Enter side a :"))
-b = float(input("Enter side b :"))
-c = float(input("Enter side c :"))
-
-if (a+b>c) and (b+c>a) and (a+c>b):
-    s = (a+b+c) / 2
-    area = math.sqrt (s*(s-a)*(s-b)*(s-c))
-    print (f"The area of the triangle is: {area:.2f} ")
-else:
-    print ("Triangular formation is not possible")    
+    print("Triangular formation is not possible") 
 
 
 
